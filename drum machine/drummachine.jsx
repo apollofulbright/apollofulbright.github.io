3d4bc36ad1e7a00caf249252d15c9593041ff2db
apollofulbright <113392852+apollofulbright@users.noreply.github.com>
2022-10-29 17:07:27 -0500

    Finished project

--- drum machine/drummachine.jsx
+++ drum machine/drummachine.jsx
@@ -1,34 +1,117 @@
 const container = document.querySelector('#drum-machine')
 const root = ReactDOM.createRoot(container)
+const sounds = [
+    {
+        key: 'Q',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+    },
+    {
+        key: 'W',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+    },
+    {
+        key: 'E',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+    },
+    {
+        key: 'A',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+    },
+    {
+        key: 'S',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+    },
+    {
+        key: 'D',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+    },
+    {
+        key: 'Z',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+    },
+    {
+        key: 'X',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+    },
+    {
+        key: 'C',
+        mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+    },
+]
 class Box extends React.Component {
+    state = {
+        currentId: ''
+    }
     constructor(props) {
         super(props)
+        this.audio = React.createRef();
+        this.playSound = this.playSound.bind(this)
+
+
+    }
+    componentDidMount() {
+        this.audio.current.addEventListener('ended', () => {
+            const parent = this.audio.current.parentNode;
+
+            parent.classList.remove('active')
+        })
+    }
+    playSound = () => {
+        this.audio.current.play()
+        const id = this.audio.current.id
+        const parent = this.audio.current.parentNode;
+        console.log(parent)
+        parent.classList.add('active')
+
+        const display = parent.parentNode;
+
+        display.querySelector('h1').innerText = `${id} is playing`
+
     }
     render() {
+        const { currentId } = this.state;
+        const { text, audio } = this.props;
         return (
-            <div className="box">{this.props.text}</div>
+            <div className="box drum-pad" onClick={this.playSound} id={this.props.mp3}>
+                {text}
+                <audio ref={this.audio} src={audio} className="clip" id={text} />
+            </div>
         )
     }
 }
 class App extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            keys: ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C']
-        }
     }
 
     render() {
-        const { keys } = this.state;
         return (
-            <div>
-                <div className="display">
-                    {keys.map((key, idx) => {
-                        return <Box text={key} key={idx} />
+            <div className="app">
+                <h1>ReactJS Drum Machine</h1>
+                <div className="display" id="display">
+                    <h1>play a sound!</h1>
+                    {sounds.map((sound, idx) => {
+                        return <Box text={sound.key} mp3={sound.mp3} key={idx} audio={sound.mp3} />
                     })}
                 </div>
             </div>
         )
     }
 }
+document.addEventListener('keydown', (e) => {
+    const id = e.key.toUpperCase();
+    const audio = document.getElementById(id);
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+        const parent = audio.parentNode
+        parent.classList.add('active')
+        audio.play();
+
+        const display = parent.parentNode;
+
+        display.querySelector('h1').innerText = `${id} is playing`
+    }
+})
 root.render(<App />)
+const boxes = document.getElementsByClassName('box')
